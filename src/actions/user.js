070f4841ed15c99d87df9b11eadd48e26880de73
arnausens <arnaust3n11@gmail.com>
2021-03-08 13:35:31 +0100

    added sign in functionality

--- src/actions/user.js
+++ src/actions/user.js
@@ -1,9 +1,25 @@
+
 import {
     SET_USER_PURCHASES,
     SET_PURCHASE_DETAIL,
     SET_CART_PRODUCTS,
-    ADD_CART_PRODUCT
+    ADD_CART_PRODUCT,
+    AUTHENTICATE_USER
 } from './types';
+
+export function signIn ({email, password}) {
+    return ({
+        type: AUTHENTICATE_USER,
+        payload: {
+            user: {
+                _id: 0,
+                name: 'Arnau Esquerré',
+                address: '1234 I live Here',
+                cartProducts: []
+            }
+        }
+    })
+}
 
 export function setPurchaseDetail(_id) {
     console.log(_id);
